c7ad777f5034db49a91ef79e58d517609a699899
Denilson <denilsob.b.sousa2021@gmail.com>
2026-04-19 14:04:26 -0300

    initial project setup

---
+++ src/main/java/br/edu/ifg/med_clinica_api/MedClinicaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.med_clinica_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedClinicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedClinicaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifg/med_clinica_api/MedClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifg.med_clinica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
