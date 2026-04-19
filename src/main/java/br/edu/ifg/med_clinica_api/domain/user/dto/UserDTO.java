749dc0145cc1ca87a65afbeda0414d8f106ad2f4
Denilson <denilsob.b.sousa2021@gmail.com>
2026-04-19 15:17:09 -0300

    feat: add user DTO validation

--- src/main/java/br/edu/ifg/med_clinica_api/domain/user/dto/UserDTO.java
+++ src/main/java/br/edu/ifg/med_clinica_api/domain/user/dto/UserDTO.java
@@ -1,7 +1,14 @@
 package br.edu.ifg.med_clinica_api.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserDTO(
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String password
 ) {
     public UserDTO(String email, String password) {
